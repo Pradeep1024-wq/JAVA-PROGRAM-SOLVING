265dd3478d1834904d9039fa4a503f276f88ef67
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-04-25 23:56:09 +0530

    Create makestring.java

---
+++ String/makestring.java
@@ -0,0 +1,9 @@
+public class makestring {
+
+	public static void main(String[] args) 
+	{
+		String a = "Pradeep";
+		String b = "Rajalakshmi Institute of Technology Studend";
+		System.out.println("THE STRING CONCTENATION :"+a+" "+b);
+	}
+}
