2cdf6a17cc3c995f6cdce5e22d81d117cebbd619
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-05-02 20:30:48 +0530

    Create constructor.java

---
+++ CONSTRUCTOR/constructor.java
@@ -0,0 +1,11 @@
+public class constructor 
+{
+	constructor()
+	{
+		System.out.println("THIS IS CONSTRUCTOR");
+	}
+	public static void main(String[] args) 
+	{
+		constructor obj = new constructor();
+	}
+}
