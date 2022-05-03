08136d4de126a150e1d2708500454728e2533c70
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-05-03 20:56:15 +0530

    Create whileloop.java

---
+++ whileloop/whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+
+	public static void main(String[] args) 
+	{
+		int num = 0;
+		while (num < 6) 
+		{
+			  System.out.println("THIS IS WHILE LOOP  "+num);
+			  num++;
+		}
+	}
+}
