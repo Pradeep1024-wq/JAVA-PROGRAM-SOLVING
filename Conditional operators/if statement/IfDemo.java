5dbbcaced2695933274c89e0da57ece6a0e2e542
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-05-06 20:55:32 +0530

    Create IfDemo.java

---
+++ Conditional operators/if statement/IfDemo.java
@@ -0,0 +1,12 @@
+class IfDemo {
+    public static void main(String args[])
+    {
+        int i = 54 ;
+ 
+        if (i < 15)
+        {
+        	System.out.println("10 is less than 15");
+        }
+        System.out.println("....... It's if statement .......");
+    }
+}
