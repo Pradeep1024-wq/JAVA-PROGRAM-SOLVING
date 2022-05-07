1a9e312469d70e2665c4c619b1a564bf9c4b7765
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-05-07 20:26:47 +0530

    Create If_Else_Demo.java

---
+++ Conditional operators/if_else_statement/If_Else_Demo.java
@@ -0,0 +1,18 @@
+public class If_Else_Demo {
+
+	public static void main(String[] args) 
+	{
+		int time = 1;
+    	if (time < 10) 
+    	{
+    	  System.out.println("Good morning.");
+    	} else if (time < 20) 
+    	{
+    	  System.out.println("Good day.");
+    	} 
+    	else 
+    	{
+    	  System.out.println("Good evening.");
+    	}
+	}
+}
