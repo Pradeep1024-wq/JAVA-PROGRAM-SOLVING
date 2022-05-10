6ebf75bf54dca803655469327de88850b42b3a37
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-05-10 20:53:10 +0530

    Create BreakExample.java

---
+++ Control Statements/BreakExample.java
@@ -0,0 +1,16 @@
+public class BreakExample 
+{  
+public static void main(String[] args) 
+{  
+     
+    for(int i=1;i<=10;i++)
+    {  
+        if(i==3)
+        {  
+            //breaking the loop  
+            break;  
+        }  
+        System.out.println(i);  
+     }  
+   }  
+}
