dd6f07fc1160a9e4d64a6a00cf85c72b4e78a995
Pradeep <85569997+Pradeep1024-wq@users.noreply.github.com>
2022-05-11 21:35:17 +0530

    Create Continue.java

---
+++ Control Statements/Continue.java
@@ -0,0 +1,18 @@
+public class Continue 
+{  
+
+	public static void main(String[] args) 
+	{    
+    
+		for(int i=1;i<=10;i++)
+		{  
+        
+			if(i==7)
+			{  
+            //using continue statement  
+            continue;  
+            }  
+        System.out.println(i);  
+        }  
+    }  
+}
